028e058ff899cb7f45ea8295238cb3cea6eb284a
rafapeixer <rafaelpeixer@icloud.com>
2024-10-13 17:34:37 -0300

    adjust isloading

--- app/page.tsx
+++ app/page.tsx
@@ -23,48 +23,38 @@ interface BinanceResponse {
 
 export default function Quotation() {
   const [result, setResult] = useState<BinanceResponse | null>(null)
-  const [error, setError] = useState<string | null>(null)
-  const [isLoading, setIsLoading] = useState(true)
   const [calculatedValue, setCalculatedValue] = useState<number | undefined>()
   const [copied, setCopied] = useState(false)
   const spreadRef = useRef<HTMLInputElement>(null)
+  const resultRef = useRef<BinanceResponse | null>(null)
 
-  const fetchCotacao = async (initialLoad = false) => {
+  const fetchCotacao = useCallback(async () => {
     try {
-      if (initialLoad) setIsLoading(true);
-      const response = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=USDTBRL");
-      const data: BinanceResponse = await response.json();
-      
-      if (result?.price !== data.price) {
-        setResult(data);
-      }
-      setError(null);
+      const response = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=USDTBRL")
+      const data: BinanceResponse = await response.json()
+      setResult(data)
+      resultRef.current = data
     } catch (err) {
       console.error(err);
-      setError("Erro ao buscar cotação");
-    } finally {
-      if (initialLoad) setIsLoading(false);
     }
-  };
-  
-  useEffect(() => {
-    fetchCotacao(true); // Chamada inicial com isLoading
-    const interval = setInterval(fetchCotacao, 3000);
-    
-    return () => clearInterval(interval);
   }, []);
-  
 
-  const onSpreadChange = useCallback(debounce((event: React.ChangeEvent<HTMLInputElement>, actualValue?: number) => {
-    const spread = Number(event?.target?.value || actualValue)
-    const currentPrice = result ? parseFloat(result.price) : 0
+  useEffect(() => {
+    fetchCotacao();
+    const interval = setInterval(fetchCotacao, 3000);
+    return () => clearInterval(interval);
+  }, [fetchCotacao]);
+
+  const onSpreadChange = debounce((event: React.ChangeEvent<HTMLInputElement>, actualValue?: number) => {
+    const spread = Number(event?.target?.value || actualValue || 0)
+    const currentPrice = resultRef.current ? parseFloat(resultRef.current.price) : 0
 
     if (!Number.isNaN(spread)) {
       setCalculatedValue(currentPrice * (1 + spread / 100))
     } else {
       setCalculatedValue(currentPrice)
     }
-  }, 800), [result])
+  }, 800);
 
   function roundToDecimalPlaces(number: number) {
     const factor = Math.pow(10, 4)
@@ -84,7 +74,7 @@ export default function Quotation() {
   useEffect(() => {
     const currentSpread = spreadRef?.current?.value
     onSpreadChange({ target: { value: currentSpread } } as React.ChangeEvent<HTMLInputElement>, Number(currentSpread))
-  }, [result, onSpreadChange])
+  }, [result])
 
   const formatedPrice = result ? parseFloat(result.price) : 0
 
@@ -92,55 +82,62 @@ export default function Quotation() {
     <Card className={styles.card}>
       <CardContent className={styles.cardContent}>
         <div className={styles.flexCenter}>
-             <>
-              <Image src="/2.png" alt="Trust Intermediações" width={240} height={80} className={styles.image} />
-              <h1 className={styles.title}>Cotação em tempo real</h1>
-              <h2 className={styles.subtitle}>Usdt da maneira mais fácil.</h2>
+          <>
+            <Image src="/2.png" alt="Trust Intermediações" width={240} height={80} className={styles.image} />
+            <h1 className={styles.title}>Cotação em tempo real</h1>
+            <h2 className={styles.subtitle}>Usdt da maneira mais fácil.</h2>
 
-              <div className={styles.inputWrapper}>
-                <Label htmlFor="currency" className={styles.inputLabel}>Moeda</Label>
-                <div className="relative">
-                  <Input id="currency" className={styles.inputField} value="Tether (USDT)" disabled />
-                </div>
+            <div className={styles.inputWrapper}>
+              <Label htmlFor="currency" className={styles.inputLabel}>Moeda</Label>
+              <div className="relative">
+                <Input id="currency" className={styles.inputField} value="Tether (USDT)" disabled />
               </div>
+            </div>
 
-              <div className={styles.inputWrapper}>
-                <Label htmlFor="quotation" className={styles.inputLabel}>Cotação</Label>
-                <div className="relative">
-                  <Input id="quotation" className={styles.inputField} value={formatedPrice.toFixed(4)} disabled />
-                </div>
+            <div className={styles.inputWrapper}>
+              <Label htmlFor="quotation" className={styles.inputLabel}>Cotação</Label>
+              <div className="relative">
+                <Input id="quotation" className={styles.inputField} value={formatedPrice.toFixed(4)} disabled />
               </div>
+            </div>
 
-              <div className={styles.inputWrapper}>
-                <Label htmlFor="spread" className={styles.inputLabel}>Spread (%)</Label>
-                <div className="relative">
-                  <Input id="spread" ref={spreadRef} className={styles.inputField} onChange={onSpreadChange} placeholder="0.5" />
-                </div>
+            <div className={styles.inputWrapper}>
+              <Label htmlFor="spread" className={styles.inputLabel}>Spread (%)</Label>
+              <div className="relative">
+                <Input
+                  id="spread"
+                  ref={spreadRef}
+                  className={styles.inputField}
+                  onChange={onSpreadChange}
+                  placeholder="0.5"
+                  defaultValue="0"
+                />
               </div>
+            </div>
 
-              <Card className={styles.cardQuotation}>
-                <CardContent className={styles.cardQuotationContent}>
-                  <div>
-                    <Label className={styles.cardQuotationLabel}>USDT Price</Label>
-                    <p className={styles.cardQuotationValue}>
-                      R$ {(calculatedValue || formatedPrice).toFixed(4)}
-                    </p>
-                  </div>
-                  <TooltipProvider>
-                    <Tooltip>
-                      <TooltipTrigger asChild>
-                        <Button className={styles.copyButton} onClick={copyValueToClipBoard}>
-                          <ClipboardIcon className="h-4 w-4" />
-                        </Button>
-                      </TooltipTrigger>
-                      <TooltipContent>
-                        <p>{copied ? "Copiado!" : "Copiar"}</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </CardContent>
-              </Card>
-            </>
+            <Card className={styles.cardQuotation}>
+              <CardContent className={styles.cardQuotationContent}>
+                <div>
+                  <Label className={styles.cardQuotationLabel}>USDT Price</Label>
+                  <p className={styles.cardQuotationValue}>
+                    R$ {(calculatedValue || formatedPrice).toFixed(4)}
+                  </p>
+                </div>
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button className={styles.copyButton} onClick={copyValueToClipBoard}>
+                        <ClipboardIcon className="h-4 w-4" />
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>{copied ? "Copiado!" : "Copiar"}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </CardContent>
+            </Card>
+          </>
         </div>
       </CardContent>
     </Card>
